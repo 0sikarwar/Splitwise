f24ef87a32bbbd7a4e4e734be18d2568c39b3ad0
Gyanender Singh <gyanendersingh5@gmail.com>
2021-09-12 15:03:37 +0530

    using hashrouter

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/home";
 import Bills from "./pages/bills";
 import Friends from "./pages/friends";
 const Routes = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/bills" component={Bills} />
       <Route exact path="/friends" component={Friends} />
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 );
 export default Routes;
